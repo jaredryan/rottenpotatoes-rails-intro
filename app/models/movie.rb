4ca763739f6c2ff2670a38f129112754af4b29c8
jaredryan <jryantennis@berkeley.edu>
2017-09-21 03:23:58 +0000

    final?

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,24 +1,5 @@
 class Movie < ActiveRecord::Base
-    @checked = ['G','PG','PG-13','R']
-    @unchecked = []
-    attr_accessor :checked
-    
     def self.ratings
         ['G','PG','PG-13','R']
-    end
-    
-    def self.check(rating)
-        if !@checked.include?(rating)
-            @checked += [rating]
-            @checked.sort!
-            @unchecked -= rating
-        end
-    end
-    
-    def self.uncheck(rating)
-        if @checked.include?(rating)
-            @checked -= rating
-            @unchecked += rating
-        end
     end
 end
